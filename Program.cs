504febb984ad9241a807c5390738b06780260145
Fabian Santillan Noriega <fabian.santillan@outlook.com>
2025-07-18 16:11:27 -0600

    Se elimina codigo comentado

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-//using ShortcutCleanerWorker;
-
-//var builder = Host.CreateApplicationBuilder(args);
-//builder.Services.AddHostedService<Worker>();
-
-//var host = builder.Build();
-//host.Run();
-
-
-
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Hosting.WindowsServices;
